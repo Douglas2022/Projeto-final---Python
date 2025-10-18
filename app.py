49609dc83e75b19bd5c13beef924dcdb54a21b15
xorionx-lab <dennis.grupoaffectio@gmail.com>
2025-10-18 19:38:45 -0300

    Fazendo a tela de login

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ app = Flask(__name__)
 
 def ConnectarBanco():
     conexao  = my.connect(
-        user = 'hoot'
-        password = password,
-        database = SuperSelectD
+        user = 'root',
+        password = '1234',
+        database = 'SuperSelectD',
+        host = 'LocalHost'
     )
     return conexao
 
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+    
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
  
-
-
-
 
 
